dd3b80a17e9ebd66a6ac92215d53386fd4f9d157
Jackson Carter <cartejac@oregonstate.edu>
2017-02-26 13:21:19 -0800

    Changed network address to work on Linux.

--- client.py
+++ client.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
 import socket
+import os
+import sys
 
+if os.name == "posix"
+    IP_ADDR = '127.0.0.1'
+elif os.name == "nt":   # Windows
+    IP_ADDR = "192.168.137.181"
+else:
+    print "Unsupported operating system."
+    sys.exit(1)
 
-TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+s.connect((IP_ADDR, TCP_PORT))
 s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
 s.close()
