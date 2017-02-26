373bedd3c5b2348bd34be184549c98529abe2559
Jackson Carter <cartejac@oregonstate.edu>
2017-02-26 14:21:08 -0800

    Implemented basic command send and receive infrastructure. Uses a newline to delimit commands and runs the pwm script when sent a pwm command.

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket
 import os
 import sys
 
-if os.name == "posix"
+if os.name == "posix":
     IP_ADDR = '10.42.0.52'
 elif os.name == "nt":   # Windows
     IP_ADDR = "192.168.137.181"
@@ -17,37 +17,44 @@ else:
 
 TCP_PORT = 50525
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+#MESSAGE = "Hello, World!"
 
 # Asks the user for an integer using the sting in prompt,
 #   checks to see if it is an integer in the specified range
 #   and prompts the user again if not
 def get_int(prompt, min_val, max_val):
     while True:
+        # Query input
         num = raw_input(prompt)
 
+        # Validate input
         try:
             num = int(num)
             if num > max_val or num < min_val:
                 print "Error: input not within range."
+                continue
         except ValueError:
             print "Error: input must be an integer."
+            continue
         return num
 
+
 if __name__ == "__main__":
+    print "PiZ Client Online!"
+
     # Open up a socket to the Pi
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((IP_ADDR, TCP_PORT))
-    s.send(MESSAGE)
+    #s.send(MESSAGE)
 
     # Ask the user for input
     while True:
-        n1 = get_int("Please input parameter 1 (integer): ")
-        n2 = get_int("Please input parameter 2 (integer): ")
-        n3 = get_int("Please input parameter 3 (integer): ")
-        n4 = get_int("Please input parameter 4 (integer): ")
+        n1 = get_int("Please input parameter 1 (integer): ", 0, 100)
+        n2 = get_int("Please input parameter 2 (integer): ", 0, 100)
+        n3 = get_int("Please input parameter 3 (integer): ", 0, 100)
+        n4 = get_int("Please input parameter 4 (integer): ", 0, 100)
 
-        s.send("command:%d %d %d %d" % (n1, n2, n3, n4))
+        s.send("pwm:%d %d %d %d\n" % (n1, n2, n3, n4))
         print "Command Sent!"
 
     #data = s.recv(BUFFER_SIZE)
--- server.py
+++ server.py
@@ -1,24 +1,64 @@
 #!/usr/bin/env python
 
 import socket
+import os
+import sys
 
 # For Windows Machines, must enable IPv4 Ping Response in Advanced Firewall Settings
- 
+
 # TCP_IP = '127.0.0.1'
 #TCP_IP = 'localhost'
 TCP_PORT = 50525 # pick something above 1024 as those are system reserved
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind((TCP_IP, TCP_PORT))
-s.bind(('',TCP_PORT))
-s.listen(1)
+# Pulls data on byte at a time from a socket until a command
+#   delimiter is received.
+# Delimiter must be a single character, if supplied
+def get_msg(s, delimiter_char='\n'):
+    msg = ""
+    try:
+        c = s.recv(1)
+        while delimiter_char not in c:
+            #print "Received: ", c,
+            msg += c
+            c = s.recv(1)
+    except KeyboardInterrupt:
+        print "Received stop signal. Terminating."
+        return None
 
-conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
-conn.close()
+    return msg
+
+def perform_command(command, args):
+    if command == "pwm":
+        print "Received 'pwm' command. Starting pwm"
+        os.system("./pwm.py %s" % args)
+    else:
+        print "Received unidentified command: '%s'. Terminating" % command
+        sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    print "PiZ Server online!"
+
+    # Open up a passive socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('',TCP_PORT))
+    s.listen(1)
+
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    while True:
+        print "About to wait for next message."
+        msg = get_msg(conn)
+        if msg == None:
+            break
+
+        print "Received message:", msg
+        command = msg.split(":")
+        perform_command(command[0], command[1])
+
+        #conn.send(msg)  # echo
+    print "Releasing sockets."
+    conn.close()
+    s.close()
