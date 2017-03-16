f05b4a154ea1fa522fdfa4eb6db9085eb2685b5d
Jordan Meader <bood64@gmail.com>
2017-03-16 15:06:52 -0700

    pwm based on inputs

--- demo.py
+++ demo.py
@@ -1,20 +1,23 @@
-# import wiringpi library to get pwm duty cycle range of 1-20xx for
-    # 0-900mV at 1mV steps control
-# wiringpi library
-# wiringpi 2 library
-# gpio library
 print("hello and welcome")
 
 print("importing libraries")
 
-# import wiringpi2 as wiringpi
-    #wiringpi2 has been deprecated
-# import RPi.GPIO as GPIO
 import sys
 import wiringpi
+import time
     # help(wiringpi) to see functions and options
 user_input = sys.argv
-print("%s" %user_input)
+start = float(user_input[1])
+high = float(user_input[2])
+low = float(user_input[3])
+end = float(user_input[4])
+
+print("user has typed in: %s" %user_input)
+print("test starts at %.3f volts" %start)
+print("test high is %.3f volts" %high)
+print("test low is %.3f volts" %low)
+print("test end at %.3f volts" %end)
+
 # Range = 2048 # sets the range of the duty cycle, 2^n bits
     # Over a voltage range of 0-900mV, this grants 0.44mV steps.
 Range = 1024 # range of 0-1023 integers, 1024 appears to do nothing
@@ -35,51 +38,44 @@ print("DutyCycle set to %i Percent" %DutyCycle)
 print("Divisor set to %i" %Divisor)
 print("Frequency set to %i Hz" %Frequency)
 
-### Pin Reference Setups
-##wiringpi.wiringPiSetupGpio()
-##wiringpi.wiringPiSetup()
-##wiringpi.wiringPiSetupPhys()
-
-# Port Setup
-# wiringpi.pinMode(port_or_pin_number, X)
-    # port_or_pin_number depends on Reference Setup
-    # X defines Port Function
-        # 0 = input, 1 = output, 2 = alternative function
-        # in this case, alt function for GPIO pin 18 is hardware PWM
-
 # Hardware PWM setups
-print "0"
+
 # Elevate Privileges
 # If using BCM GPIO NUMBERS
 wiringpi.wiringPiSetupGpio() # must run with root privileges!
-print "0.5"
+
 wiringpi.pinMode(18,2) # X = 2 declares alt function, pwm only works on GPIO port 18
 # set PWM Mode
     # balanced (default, varies frequency with duty cycle for even pulses)
     # mark-space mode (traditional)
-print "1"
+
 wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
 wiringpi.pwmSetRange(Range) # sets the range register for the PWM. Default is 1024.
 wiringpi.pwmSetClock(Divisor) # sets divisor for the PWM clock
 wiringpi.pwmWrite(18,DutyCalc) # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
-print "2"
+
 Volts = 3.3*DutyCalc/Range
-Sensor = 0.9*DutyCalc/Range
+Sensor = 0.4*DutyCalc/Range
 print("Pin 18 is set to %g Volts" %Volts)
 print("Past the Op Amp, sensor voltage calculates to %g Volts" %Sensor)
+
+raw_input("Press [Enter] to begin test.")
+# Prep for test
+begin = int(start*1000)
+finish = int(end*1000)
+DutyCalc = (start*Range)/(0.4)# has range of Range
+print DutyCalc
+print "start %0.3f volts" %start
+# Test
+for i in range(begin, finish+1):
+	print "i %i in loop" %i
+	print "DutyCalc %i of 1024" %DutyCalc
+	DutyCalc = ((start+(i*0.001))*Range)/(0.4)# has range of Range
+	voltage = DutyCalc*0.4/Range
+	# pass in variable for time delay from GUI
+	time.sleep(0.040)
+	wiringpi.pwmWrite(18,int(DutyCalc))
+	print "voltage is %0.3f" %voltage
+
 raw_input("Press [Enter] to stop PWM.")
 wiringpi.pwmWrite(18,0) # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
-
-### OR, using wiringpi numbers
-##wiringpi.wiringPiSetup()
-##wiringpi.pinMode(1,2) # pwm only works on wiringpi pin 1
-##wiringpi.pwmWrite(1,0) # duty cycle between 0 and 1024. 0 = off, 1024 = fully on  # duty 
-##
-### OR, using P1 header pin numbers
-##wiringpi.wiringPiSetupPhys()
-##wiringpi.pinMode(12,2) # pwm only works on P1 header pin 12
-##wiringpi.pwmWrite(12,0) # duty cycle between 0 and 1024. 0 = off, 1024 = fully on
-
-#stop PWM by changing pin to input or output
-##wiringpi.pinMode(18,0)
-
