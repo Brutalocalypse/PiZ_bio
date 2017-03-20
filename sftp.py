618a743ef7385d0101174f484a0fdc7e92bf7f67
Brutalocalypse <Brutalocalypse@users.noreply.github.com>
2017-03-20 14:41:33 -0700

    switched to ipaddress as hostname error
    os.system wasn't seeing the hostname although keys are provided and hostname was identified when ran in terminal

--- sftp.py
+++ sftp.py
@@ -6,4 +6,12 @@ import os
 # scp allows uploading of files from the commandline
 # Input: a local path to the file to upload
 def upload_file(fpath):
-	os.system("scp %s brutal:Desktop/" % fpath)
+	# os.system("scp %s brutal:Desktop/" % fpath) # Desktop connection
+	
+	#cmd = "sh -c 'scp %s venom:Desktop/'" % fpath
+	#print "'", cmd, "'"
+	#os.system("%s" %cmd) # Laptop connection
+		# bug where hostname isn't recognized even though it can be used in terminal just fine
+
+	os.system("scp %s LethalInjection@192.168.137.1:Desktop/" %fpath) # Laptop connection
+		# requires password prompt
